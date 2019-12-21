1fb8ee232fb351b79f26ec967b125abd4db9c741
Matthijs Tempels <matthijs@townsville.nl>
2019-12-21 10:30:44 +0100

    moved to node 12

--- test/test.index.js
+++ test/test.index.js
@@ -7,17 +7,16 @@
 
 "use strict";
 
-var assert = require('assert');
-var async = require('async');
-var fs = require('fs');
-var logger = require.main.require('lib/index');
-var sinon = require('sinon');
-var Syslog = require('winston-syslog').Syslog;
-var util = require('util');
+const assert = require('assert');
+const async = require('async');
+const fs = require('fs');
+const logger = require('../lib/index');
+const util = require('util');
 
 
 // Log file to use
-var LOG_FILE = '/tmp/townsville_logger_test.log';
+const LOG_FILE = '/tmp/cs_logger_test.log';
+
 
 /**
  * Helper function to remove existing log files
@@ -29,7 +28,7 @@ function removeLogFiles(done) {
   var parts = LOG_FILE.split('.');
 
   // Construct file paths
-  for(var i = 0; i <= 20; i++) {
+  for (let i = 0; i <= 20; i++) {
     if (i === 0) {
       files.push(LOG_FILE);
     } else {
@@ -41,15 +40,14 @@ function removeLogFiles(done) {
 
   async.eachSeries(
     files,
-    function(file, cb) {
-      fs.exists(file, function(exists) {
+    (file, cb) => {
+      fs.exists(file, (exists) => {
         if (exists) {
           fs.unlink(file, (err) => {
             if (err) {
-              assert(err);
+              assert(err, "Unlink files failed");
             }
-          }
-          );
+          });
         }
         cb();
       });
@@ -60,7 +58,7 @@ function removeLogFiles(done) {
 }
 
 
-describe('The townsville logger', function() {
+describe('The cs logger', () => {
 
   // Ditch log file before
   beforeEach(removeLogFiles);
@@ -69,24 +67,36 @@ describe('The townsville logger', function() {
   afterEach(removeLogFiles);
 
 
-  it('throws the proper error if not initialized', function() {
+  it('throws the proper error if not initialized', () => {
 
     /*jshint loopfunc: true*/
-    var inst = logger.createLogger('aap');
+    const inst = logger.createLogger('aap');
 
-    for (var i = 0; i < 5; i++) {
-      assert.throws(function() {
-        switch(i) {
-          case 0: inst.fatal('fatal'); break;
-          case 1: inst.error('error'); break;
-          case 2: inst.warn('warn'); break;
-          case 3: inst.info('info'); break;
-          case 4: inst.debug('debug'); break;
-          case 5: inst.trace('trace'); break;
+    for (let i = 0; i < 5; i++) {
+      assert.throws(() => {
+        switch (i) {
+          case 0:
+            inst.fatal('fatal');
+            break;
+          case 1:
+            inst.error('error');
+            break;
+          case 2:
+            inst.warn('warn');
+            break;
+          case 3:
+            inst.info('info');
+            break;
+          case 4:
+            inst.debug('debug');
+            break;
+          case 5:
+            inst.trace('trace');
+            break;
         }
-      }, function(err) {
+      }, (err) => {
         if ((err instanceof Error) &&
-          /Log system is not initialized/.test(err) ) {
+          /Log system is not initialized/.test(err)) {
           return true;
         }
         return false;
@@ -94,12 +104,12 @@ describe('The townsville logger', function() {
     }
   });
 
-  it('supports the required interface', function() {
+  it('supports the required interface', () => {
 
     logger.deinit();
     logger.init({});
 
-    var inst = logger.createLogger('aap');
+    const inst = logger.createLogger('aap');
 
     inst.fatal('fatal');
     inst.error('error');
@@ -116,7 +126,7 @@ describe('The townsville logger', function() {
     inst.isTrace();
   });
 
-  it('logs the expected results', function(done) {
+  it('logs the expected results', (done) => {
 
     // Create file logger
     logger.deinit();
@@ -134,7 +144,7 @@ describe('The townsville logger', function() {
     });
 
     // Create log instance
-    var inst = logger.createLogger('mymodule');
+    const inst = logger.createLogger('mymodule');
     assert(inst.isFatal(), 'Expected isFatal true');
     assert(inst.isError(), 'Expected isError true');
     assert(inst.isWarn(), 'Expected isWarn true');
@@ -155,21 +165,25 @@ describe('The townsville logger', function() {
 
     // Test appending
     inst.debug('test %d append', 123, '[some]');
+    inst.debug('test another append', 0);
+    inst.debug('test object append', {
+      teun: 1,
+      toon: 'august',
+    });
 
     // Another logger
-    var inst2 = logger.createLogger('myother');
+    const inst2 = logger.createLogger('myother');
     inst2.debug('check');
 
     // Some time to flush
     setTimeout(
-      function() {
+      () => {
 
         // Read file
-        fs.readFile(LOG_FILE, function(err, data) {
+        fs.readFile(LOG_FILE, 'utf8', (err, data) => {
           if (err) {
             throw err;
           }
-
           assert.equal(
             data,
             'fatal: [mylog.mymodule] fatal\n' +
@@ -180,6 +194,8 @@ describe('The townsville logger', function() {
             'trace: [mylog.mymodule] trace\n' +
             'debug: [mylog.mymodule] aap test noot 24 mies\n' +
             'debug: [mylog.mymodule] test 123 append [some]\n' +
+            'debug: [mylog.mymodule] test another append 0\n' +
+            'debug: [mylog.mymodule] test object append {"teun":1,"toon":"august"}\n' +
             'debug: [mylog.myother] check\n',
             'log file not as expected');
           done();
@@ -189,7 +205,7 @@ describe('The townsville logger', function() {
     );
   });
 
-  it('handles module levels properly', function(done) {
+  it('handles module levels properly', (done) => {
 
     // Create file logger
     logger.deinit();
@@ -209,9 +225,9 @@ describe('The townsville logger', function() {
     });
 
     // Create log instances
-    var aap = logger.createLogger('aap');
-    var noot = logger.createLogger('noot');
-    var mies = logger.createLogger('mies');
+    const aap = logger.createLogger('aap');
+    const noot = logger.createLogger('noot');
+    const mies = logger.createLogger('mies');
 
     // Check aap levels
     assert(aap.isFatal());
@@ -241,7 +257,7 @@ describe('The townsville logger', function() {
   });
 
 
-  it('logs without prefixes', function(done) {
+  it('logs without prefixes', (done) => {
 
     // Create file logger
     logger.deinit();
@@ -257,31 +273,27 @@ describe('The townsville logger', function() {
     });
 
     // Create log instance
-    var inst = logger.createLogger('mymodule');
+    const inst = logger.createLogger('mymodule');
     inst.info('info');
 
     // Some time to flush
-    setTimeout(
-      function() {
+    setTimeout(() => {
+      // Read file
+      fs.readFile(LOG_FILE, (err, data) => {
+        if (err) {
+          throw err;
+        }
 
-        // Read file
-        fs.readFile(LOG_FILE, function(err, data) {
-          if (err) {
-            throw err;
-          }
-
-          var str = data.toString('ascii');
-          var expect = 'info: info\n';
-          assert.equal(str, expect, 'log file not as expected');
-          done();
-        });
-      },
-      500
-    );
+        const str = data.toString('ascii');
+        const expect = 'info: info\n';
+        assert.equal(str, expect, 'log file not as expected');
+        done();
+      });
+    }, 500);
   });
 
 
-  it('logs with name prefix', function(done) {
+  it('logs with name prefix', (done) => {
 
     // Create file logger
     logger.deinit();
@@ -297,31 +309,27 @@ describe('The townsville logger', function() {
     });
 
     // Create log instance
-    var inst = logger.createLogger('mymodule');
+    const inst = logger.createLogger('mymodule');
     inst.info('info');
 
     // Some time to flush
-    setTimeout(
-      function() {
+    setTimeout(() => {
+      // Read file
+      fs.readFile(LOG_FILE, (err, data) => {
+        if (err) {
+          throw err;
+        }
 
-        // Read file
-        fs.readFile(LOG_FILE, function(err, data) {
-          if (err) {
-            throw err;
-          }
-
-          var str = data.toString('ascii');
-          var expect = 'info: [mymodule] info\n';
-          assert.equal(str, expect, 'log file not as expected');
-          done();
-        });
-      },
-      500
-    );
+        const str = data.toString('ascii');
+        const expect = 'info: [mymodule] info\n';
+        assert.equal(str, expect, 'log file not as expected');
+        done();
+      });
+    }, 500);
   });
 
 
-  it('logs with pid prefix', function(done) {
+  it('logs with pid prefix', (done) => {
 
     // Create file logger
     logger.deinit();
@@ -337,31 +345,27 @@ describe('The townsville logger', function() {
     });
 
     // Create log instance
-    var inst = logger.createLogger('mymodule');
+    const inst = logger.createLogger('mymodule');
     inst.info('info');
 
     // Some time to flush
-    setTimeout(
-      function() {
+    setTimeout(() => {
+      // Read file
+      fs.readFile(LOG_FILE, (err, data) => {
+        if (err) {
+          throw err;
+        }
 
-        // Read file
-        fs.readFile(LOG_FILE, function(err, data) {
-          if (err) {
-            throw err;
-          }
-
-          var str = data.toString('ascii');
-          var expect = util.format('info: [#%d] info\n', process.pid);
-          assert.equal(str, expect, 'log file not as expected');
-          done();
-        });
-      },
-      500
-    );
+        const str = data.toString('ascii');
+        const expect = util.format('info: [#%d] info\n', process.pid);
+        assert.equal(str, expect, 'log file not as expected');
+        done();
+      });
+    }, 500);
   });
 
 
-  it('logs with pid and name prefix', function(done) {
+  it('logs with pid and name prefix', (done) => {
 
     // Create file logger
     logger.deinit();
@@ -377,31 +381,27 @@ describe('The townsville logger', function() {
     });
 
     // Create log instance
-    var inst = logger.createLogger('mymodule');
+    const inst = logger.createLogger('mymodule');
     inst.info('info');
 
     // Some time to flush
-    setTimeout(
-      function() {
+    setTimeout(() => {
+      // Read file
+      fs.readFile(LOG_FILE, (err, data) => {
+        if (err) {
+          throw err;
+        }
 
-        // Read file
-        fs.readFile(LOG_FILE, function(err, data) {
-          if (err) {
-            throw err;
-          }
-
-          var str = data.toString('ascii');
-          var expect = util.format('info: [#%d-mymodule] info\n', process.pid);
-          assert.equal(str, expect, 'log file not as expected');
-          done();
-        });
-      },
-      500
-    );
+        const str = data.toString('ascii');
+        const expect = util.format('info: [#%d-mymodule] info\n', process.pid);
+        assert.equal(str, expect, 'log file not as expected');
+        done();
+      });
+    }, 500);
   });
 
 
-  it('logs a timestamp properly', function(done) {
+  it('logs a timestamp properly', (done) => {
 
     // Create file logger
     logger.deinit();
@@ -415,31 +415,27 @@ describe('The townsville logger', function() {
     });
 
     // Create log instance
-    var inst = logger.createLogger('mymodule');
+    const inst = logger.createLogger('mymodule');
     inst.info('test123');
 
     // Some time to flush
-    setTimeout(
-      function() {
+    setTimeout(() => {
+      // Read file
+      fs.readFile(LOG_FILE, (err, data) => {
+        if (err) {
+          throw err;
+        }
 
-        // Read file
-        fs.readFile(LOG_FILE, function(err, data) {
-          if (err) {
-            throw err;
-          }
-
-          var str = data.toString('ascii');
-          // Check with regex
-          assert(str.match(/\d{8}-\d{6}[.]\d{3} - info: \[mymodule\] test123\n$/g),
-            'log file not as expected');
-          done();
-        });
-      },
-      500
-    );
+        const str = data.toString('ascii');
+        // Check with regex
+        assert(str.match(/\d{8}-\d{6}[.]\d{3} - info: \[mymodule\] test123\n$/g),
+          'log file not as expected');
+        done();
+      });
+    }, 500);
   });
 
-  it('handles rolling file correctly', function(done) {
+  it('handles rolling file correctly', (done) => {
 
     // Create file logger
     logger.deinit();
@@ -457,87 +453,35 @@ describe('The townsville logger', function() {
     });
 
     // Create log instance
-    var inst = logger.createLogger('mymodule');
+    const inst = logger.createLogger('mymodule');
 
-    var i = 0;
-    var interval = setInterval(
-      function() {
-        if (i < 5) {
-          inst.info('aap', i);
-          i++;
-        } else {
-          clearInterval(interval);
-          var files = [
-            '/tmp/townsville_logger_test.log',
-            '/tmp/townsville_logger_test1.log',
-            '/tmp/townsville_logger_test2.log',
-            '/tmp/townsville_logger_test3.log',
-            '/tmp/townsville_logger_test4.log',
-          ];
+    let i = 0;
+    const interval = setInterval(() => {
+      if (i < 5) {
+        inst.info('aap', i);
+        i++;
+      } else {
+        clearInterval(interval);
+        const files = [
+          '/tmp/cs_logger_test.log',
+          '/tmp/cs_logger_test1.log',
+          '/tmp/cs_logger_test2.log',
+          '/tmp/cs_logger_test3.log',
+          '/tmp/cs_logger_test4.log',
+        ];
 
-          async.forEach(
-            files,
-            function(file, cb) {
-              fs.exists(file, function(exists) {
-                assert(exists, file + ' should exist');
-                cb();
-              });
-            },
-            function() {
-              done();
-            }
-          );
-        }
-      },
-      10
-    );
-  });
-
-  it('logs to syslog', function(done) {
-
-    // Set spy on syslog transport log method
-    var logSpy = sinon.spy(Syslog.prototype, 'log');
-
-    // Create file logger
-    logger.deinit();
-    logger.init({
-      name: 'mylog',
-      level: 'trace',
-      showName: true,
-      syslog: {
+        async.forEach(
+          files,
+          (file, cb) => {
+            fs.exists(file, (exists) => {
+              assert(exists, file + ' should exist');
+              cb();
+            });
+          },
+          done
+        );
       }
-    });
-
-    // Create log instance
-    var inst = logger.createLogger('mymodule');
-
-    inst.fatal('fatal');
-    inst.error('error');
-    inst.warn('warn');
-    inst.info('info');
-    inst.debug('debug');
-    inst.trace('trace');
-
-    setTimeout(
-      function() {
-
-        assert.equal(logSpy.callCount, 6, 'expected 6 log calls');
-        assert.equal(logSpy.args[0][0], 'crit', 'expected "crit" level');
-        assert.equal(logSpy.args[0][1], '[mymodule] fatal', 'expected fatal');
-        assert.equal(logSpy.args[1][0], 'error', 'expected "error" level');
-        assert.equal(logSpy.args[1][1], '[mymodule] error', 'expected error');
-        assert.equal(logSpy.args[2][0], 'warning', 'expected "warning" level');
-        assert.equal(logSpy.args[2][1], '[mymodule] warn', 'expected warn');
-        assert.equal(logSpy.args[3][0], 'info', 'expected "info" level');
-        assert.equal(logSpy.args[3][1], '[mymodule] info', 'expected info');
-        assert.equal(logSpy.args[4][0], 'debug', 'expected "debug" level');
-        assert.equal(logSpy.args[4][1], '[mymodule] debug', 'expected debug');
-        assert.equal(logSpy.args[5][0], 'debug', 'expected "debug" level');
-        assert.equal(logSpy.args[5][1], '[mymodule] trace', 'expected trace');
-        done();
-      },
-      500
-    );
+    }, 10);
   });
 
 });
